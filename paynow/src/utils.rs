33efd485dfe62568e99f01b6e42dedddc7f35eea
saebs <sabelo.n@yandex.com>
2019-12-27 08:58:37 +0200

    hash make function

--- paynow/src/utils.rs
+++ paynow/src/utils.rs
@@ -4,22 +4,20 @@ Author: Sabelo Ntabeni
 email: sabelo.n@yandex.com
 *******************************/ 
 
+/* Collection of General helper functions or utilities */
 
-// Build hasher helper
+use sha2::{Sha512, Digest};
 
-//      Concat values in message for each element in their raw form
-
-//      append intergration key
-
-//      UTF8 encode the string (May not be neccessary in Rust, Strings are UTF8 encoded by default)
-
-
-//      create a SHA512 hash of the string
-
-//      output or return result in Uppercase hexadecimall
-
-// Init Response Constructor
+/// Generate a hash form two UTF8 strings "message" and 'intergration key'
+pub fn hash_make<'a>(message: &'a str, intergration_key: &'static str) ->  String {
+    // concat to key to end of message
+    let mut msg = String::new();
+    msg.push_str(message);
+    msg.push_str(intergration_key);
+    // create sha512 hash of result and convert to uppercase hexadecimal
+    let hash: &[u8]= msg.as_ref();
+    format!("{:X}", Sha512::digest(hash))
+}
 
 
-// Get Data from Paynow
-
+// TODO hash_verify
