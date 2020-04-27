719bf470eb9e1a1ecba6834db117b82db20d7347
saebs <ntabenism@gmail.com>
2020-04-27 05:47:43 +0000

    hash generator tests

--- paynow/src/utils.rs
+++ paynow/src/utils.rs
@@ -9,6 +9,8 @@ Permission to use, copy, modify, and/or distribute this software for any purpose
 
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
+
+
 /* Collection of General helper functions or utilities */
 
 use sha2::{Digest, Sha512};
@@ -16,14 +18,14 @@ use std::num::ParseFloatError;
 
 /// Generate a hash form two UTF8 strings "message" and 'intergration key'
 #[allow(dead_code)]
-pub fn hash_make<'a>(message: &'a str, intergration_key: &'static str) -> String {
+pub fn hash_gen(message: &str, integration_key: &str) -> Result<String, &'static str> {
     // concat to key to end of message
     let mut msg = String::new();
     msg.push_str(message);
-    msg.push_str(intergration_key);
+    msg.push_str(integration_key);
     // create sha512 hash of result and convert to uppercase hexadecimal
     let hash: &[u8] = msg.as_ref();
-    format!("{:X}", Sha512::digest(hash))
+    Ok(format!("{:X}", Sha512::digest(hash)))
 }
 
 
@@ -44,6 +46,12 @@ mod tests {
     // Prove that 1 ->  ~2 
         assert_eq!(1000usize , to_cents("10").unwrap());
         assert_eq!(1000usize , to_cents(&"10".to_string()).unwrap());
+    }
+
+    #[test]
+    fn hash_util() {
+        let message = "1201TEST REF99.99A test ticket transactionhttp://www.google.com/search?q=returnurlhttp://www.google.com/search?q=resulturlMessage";
+        assert_eq!(hash_gen(message, "3e9fed89-60e1-4ce5-ab6e-6b1eb2d4f977").unwrap(), "2A033FC38798D913D42ECB786B9B19645ADEDBDE788862032F1BD82CF3B92DEF84F316385D5B40DBB35F1A4FD7D5BFE73835174136463CDD48C9366B0749C689");
     }
 }
 
