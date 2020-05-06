c2c1441e782ad051de0cbfb682c42ec46ad43b75
saebs <ntabenism@gmail.com>
2020-05-06 17:51:46 +0200

    hash gen issues

--- paynow/src/utils.rs
+++ paynow/src/utils.rs
@@ -20,56 +20,68 @@ pub fn hash_gen(message: &str, integration_key: &str) -> Result<String, &'static
     Ok(format!("{:X}", Sha512::digest(hash)))
 }
 
-/// helper for parsing initResponse for checking if hash valid 
+/// helper for parsing initResponse for checking if hash valid
 /// collates all non-hash values and extracts the response hash
 /// output: (non-hash values string, extracted hash)
-fn parse_response_values(input: &str) ->  (String, String) {
+fn parse_response_values(input: &str) -> (String, String) {
     // input.chars().collect::<String>()
     let mut values_str = String::new();
-    let mut hash = String::new();
     let mut key = String::new();
     const AMPERSAND: char = '&';
     const EQUALSIGN: char = '=';
     const HASH_LABEL: &str = "hash";
-    
+
     let mut key_mode = true;
     for c in input.chars() {
-        // We dont wnat to collate the '&' or '=' but we need to recognise them
+        // We dont want to collate the '&' or '=' but we need to recognise them
         // to switch state
         // we want to seperate the hash value as well
-        match  c {
-            AMPERSAND => {key_mode = true;
-                key = String::new();},
-            EQUALSIGN => {key_mode = false; key = String::new()},
-            _ => {},
+        match c {
+            AMPERSAND => {
+                key_mode = true;
+                key = String::new();
+                continue;
+            }
+
+            EQUALSIGN => {
+                key_mode = false;
+                continue;
+            }
+            _ => { 
+                if key_mode && key.to_lowercase() != HASH_LABEL {
+                    values_str.push(c);
+                } else 
+                {
+                    key.push(c);
+                } 
+            }
         }
 
-        if c != AMPERSAND  && c != EQUALSIGN && !key_mode && key.to_lowercase() != HASH_LABEL {
-            values_str.push(c);
-        } else if c != AMPERSAND  && c != EQUALSIGN && !key_mode && key.to_lowercase() == HASH_LABEL   {
-            hash.push(c);
-        } else {
-            key.push(c);
-        }  
-    } 
+    }
 
-    (values_str, hash) 
+    (values_str, key)
 }
 
-/// Checks if Response hash value is valid as a security measure 
+/// Checks if Response hash value is valid as a security measure
 /// Assumption: The message has been url decoded
-pub fn is_valid_hash(response_message: &str, integration_key: &str) -> Result<(bool, String), &'static str> {
-    // need to sanitize response string 
+pub fn is_valid_hash(
+    response_message: &str,
+    integration_key: &str,
+) -> Result<(bool, String), &'static str> {
+    // need to sanitize response string
 
     let (message, hash) = parse_response_values(response_message);
-    let hash_local = hash_gen(&message, integration_key); 
-    Ok((hash.as_str() == hash_local.unwrap().as_str(), hash))
+    let hash_local = hash_gen(&message, integration_key);
+    let two = hash_local.unwrap().clone();
+    let one = hash.as_str();
+    assert_eq!(one, two);
+    Ok((hash.as_str() == two, hash))
 }
 
 /// Parse string input to cents
 /// The inner represantion of all money or currency is handled in cents
 /// eg an input of 1 ZWL dollar, is represented as 100 cents in memory calculations
-// This implementation may change in the Transactions to use other Currency libraries 
+// This implementation may change in the Transactions to use other Currency libraries
 // its still nice to have this function tho
 pub fn to_cents(amt: &str) -> Result<usize, ParseFloatError> {
     // parse each numerick
@@ -77,16 +89,14 @@ pub fn to_cents(amt: &str) -> Result<usize, ParseFloatError> {
     Ok((amt * 100f64) as usize)
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use crate::utils::*;
     #[test]
     fn parse_to_cents() {
-    // Prove that 1 ->  ~2 
-        assert_eq!(1000usize , to_cents("10").unwrap());
-        assert_eq!(1000usize , to_cents(&"10".to_string()).unwrap());
+        // Prove that 1 ->  ~2
+        assert_eq!(1000usize, to_cents("10").unwrap());
+        assert_eq!(1000usize, to_cents(&"10".to_string()).unwrap());
     }
 
     #[test]
@@ -99,10 +109,12 @@ mod tests {
     fn validates_hash() {
         //TODO 27-04-2020
         let integration_key = "3e9fed89-60e1-4ce5-ab6e-6b1eb2d4f977";
-        let eg_response = "status=Ok&browserurl=https%3a%2f%2fstaging.paynow.co.zw%2fPayment%2fConfirmPayment%2f9510&pollurl=https%3a%2f%2fstaging.paynow.co.zw%2fInterface%2fCheckPayment%2f%3fguid%3dc7ed41da-0159-46da-b428-69549f770413&paynowreference=9510&hash=750DD0B0DF374678707BB5AF915AF81C228B9058AD57BB7120569EC68BBB9C2EFC1B26C6375D2BC562AC909B3CD6B2AF1D42E1A5E479FFAC8F4FB3FDCE71DF4D";
-        let hash_expected = "750DD0B0DF374678707BB5AF915AF81C228B9058AD57BB7120569EC68BBB9C2EFC1B26C6375D2BC562AC909B3CD6B2AF1D42E1A5E479FFAC8F4FB3FDCE71DF4D";
-        assert_eq!(is_valid_hash(eg_response , integration_key).unwrap(), (true, hash_expected.to_string()));
+        // let _eg_response = "status=Ok&browserurl=https%3a%2f%2fstaging.paynow.co.zw%2fPayment%2fConfirmPayment%2f9510&pollurl=https%3a%2f%2fstaging.paynow.co.zw%2fInterface%2fCheckPayment%2f%3fguid%3dc7ed41da-0159-46da-b428-69549f770413&paynowreference=9510&hash=750DD0B0DF374678707BB5AF915AF81C228B9058AD57BB7120569EC68BBB9C2EFC1B26C6375D2BC562AC909B3CD6B2AF1D42E1A5E479FFAC8F4FB3FDCE71DF4D";
+        let response = "status=Ok&browserurl=https://staging.paynow.co.zw/Payment/ConfirmPayment/9510&pollurl=https://staging.paynow.co.zw/Interface/CheckPayment/?guid=c7ed41da-0159-46da-b428-69549f770413&paynowreference=9510&hash=750DD0B0DF374678707BB5AF915AF81C228B9058AD57BB7120569EC68BBB9C2EFC1B26C6375D2BC562AC909B3CD6B2AF1D42E1A5E479FFAC8F4FB3FDCE71DF4D";
+        let hash_o = "750DD0B0DF374678707BB5AF915AF81C228B9058AD57BB7120569EC68BBB9C2EFC1B26C6375D2BC562AC909B3CD6B2AF1D42E1A5E479FFAC8F4FB3FDCE71DF4D";
+        assert_eq!(
+            is_valid_hash(response, integration_key).unwrap(),
+            (true, hash_o.to_string())
+        );
     }
 }
-
-
